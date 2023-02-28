c8b192ce48b1ac76e68f8bb4fdf013467c747857
Deimar <deimarparn14@gmail.com>
2023-02-28 19:13:48 +0200

    Created navbar component

---
+++ ecommerce-vue-app/src/Components/Nav.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="navbar-container">
+       <div class="logo-section">
+           <img src="public/images/logo.svg" />
+       </div>
+       <nav>
+           <ul>
+               <li><a href="#">Collections</a></li>
+               <li><a href="#">Men</a></li>
+               <li><a href="#">Women</a></li>
+               <li><a href="#">About</a></li>
+               <li><a href="#">Contact</a></li>
+           </ul>
+       </nav>
+       <div class="cart-section">
+           <img src="public/images/icon-cart.svg" />
+           <img src="public/images/image-avatar.png" />
+       </div>
+   </div>
+</template>
+
+<script>
+export default {
+    name: 'Nav'
+}
+</script>
+
+<style lang="scss">
+@import './Nav.scss';
+</style>
---
+++ ecommerce-vue-app/src/Pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+     <div class="home-container">
+        <Nav />
+    </div>
+</template>
+
+<script>
+import Nav from '../Components/Nav.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Nav
+  }
+}
+</script>
